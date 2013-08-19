87cfc96fd9a0d27aea49a5dd426e2713fdaa932b
Hayashi Soichi <hayashis@osg-xsede.grid.iu.edu>
2013-08-19 16:43:57 +0000

    replaced split script with existing split script used by osg-blast

---
+++ split.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import sys
+
+if len(sys.argv) == 3:
+    input_file = sys.argv[1]
+    outdir = sys.argv[2]
+else:
+    print("usage: ./split.py input_file outdir")
+    sys.exit(1)
+
+#parse input query
+input = open(input_file)
+queries = []
+query = ""
+name = ""
+for line in input.readlines():
+    if line[0] == ">":
+        if name != "":
+            queries.append([name, query])
+        name = line
+        query = ""
+    else:
+        query += line
+if name != "":
+    queries.append([name, query])
+input.close()
+
+#output each files
+block = {}
+count = 0
+for query in queries:
+    outfile = open(outdir+"/"+str(count)+".fasta", "w")
+    outfile.write(query[1])
+    outfile.close()
+    count+=1
